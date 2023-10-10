15b436f0b8ec85bbb3c9c0c039426f41d004a120
Maria <mvalenciacuberos@gmail.com>
2023-10-10 11:26:01 +0200

    Continue setup for  migrations

--- database/databaseExample.mjs
+++ database/databaseExample.mjs
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import postgres from 'postgres';
+import { setEnvironmentVariables } from '../util/config.mjs';
 
 dotenv.config();
+setEnvironmentVariables();
 
 const sql = postgres();
